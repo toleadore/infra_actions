11c4f6acbc7732121a6b961286ec9dbebbfb8753
Anatoliy Serganov <toleadore@mail.ru>
2022-09-19 20:45:27 +0300

    1.2

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     """Главная"""
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось!БИИЧ!')
 
 
 def second_page(request):
